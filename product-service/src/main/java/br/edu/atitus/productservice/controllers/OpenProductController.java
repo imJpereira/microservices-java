ac737451015fc06b8c31c7af7fbf5df2183472c6
joaopereira <jps.pereira0805@gmail.com>
2025-05-29 20:57:25 -0300

    configurações de cache, porém o cache ainda não está funcionando por algum motivo

--- product-service/src/main/java/br/edu/atitus/productservice/controllers/OpenProductController.java
+++ product-service/src/main/java/br/edu/atitus/productservice/controllers/OpenProductController.java
@@ -5,6 +5,7 @@ import br.edu.atitus.productservice.clients.CurrencyResponse;
 import br.edu.atitus.productservice.entities.ProductEntity;
 import br.edu.atitus.productservice.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,10 +18,12 @@ public class OpenProductController {
 
     private final ProductRepository productRepository;
     private final CurrencyClient currencyClient;
+    private final CacheManager cacheManager;
 
-    public OpenProductController(ProductRepository productRepository, CurrencyClient currencyClient) {
+    public OpenProductController(ProductRepository productRepository, CurrencyClient currencyClient, CacheManager cacheManager) {
         this.productRepository = productRepository;
         this.currencyClient = currencyClient;
+        this.cacheManager = cacheManager;
     }
 
     @Value("${server.port}")
@@ -29,23 +32,33 @@ public class OpenProductController {
     @GetMapping("/{idProduct}/{targetCurrency}")
     public ResponseEntity<ProductEntity> getById(@PathVariable Long idProduct, @PathVariable String targetCurrency) throws Exception {
 
-        ProductEntity product = productRepository.findById(idProduct)
-                .orElseThrow(() -> new Exception("Product not found"));
+        targetCurrency = targetCurrency.toUpperCase();
+        String nameCache = "Product";
+        String keyCache = idProduct + "_" + targetCurrency;
 
-        product.setEnvironment("" + serverPort);
+        ProductEntity product = cacheManager.getCache(nameCache).get(keyCache, ProductEntity.class);
 
-        if (targetCurrency.equals(product.getCurrency())) {
-            product.setConvertedPrice(product.getPrice());
-        } else {
-            CurrencyResponse currency = currencyClient.getCurrency(product.getPrice(), product.getCurrency(), targetCurrency);
+        if (product == null) {
+            product = productRepository.findById(idProduct)
+                    .orElseThrow(() -> new Exception("Product not found"));
 
-            if (currency != null) {
-                product.setEnvironment("service: product - port: "+product.getEnvironment() + " // "+ currency.getEnvironment());
-                product.setConvertedPrice(currency.getConvertedValue());
+            product.setEnvironment("" + serverPort);
+
+            if (targetCurrency.equals(product.getCurrency())) {
+                product.setConvertedPrice(product.getPrice());
             } else {
-                product.setConvertedPrice(-1);
-                product.setEnvironment("service: product - port: "+product.getEnvironment() + " // Currency unavailable");
+                CurrencyResponse currency = currencyClient.getCurrency(product.getPrice(), product.getCurrency(), targetCurrency);
+
+                if (currency != null) {
+                    product.setEnvironment("service: product - port: " + product.getEnvironment() + " // " + currency.getEnvironment());
+                    product.setConvertedPrice(currency.getConvertedValue());
+                } else {
+                    product.setConvertedPrice(-1);
+                    product.setEnvironment("service: product - port: " + product.getEnvironment() + " // Currency unavailable");
+                }
             }
+        } else {
+            product.setEnvironment("service: product - port:" + serverPort + " (cache) // " + targetCurrency);
         }
 
         return ResponseEntity.ok(product);
