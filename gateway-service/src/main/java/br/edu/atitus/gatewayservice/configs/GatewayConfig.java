ace9288ad761df1a3a990586e4e8eddf4e0115dc
joaopereira <jps.pereira0805@gmail.com>
2025-06-07 16:49:03 -0300

    gateway-service

---
+++ gateway-service/src/main/java/br/edu/atitus/gatewayservice/configs/GatewayConfig.java
@@ -0,0 +1,30 @@
+package br.edu.atitus.gatewayservice.configs;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    RouteLocator getRoutes(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("Authorization", "Bearer token")
+                                .addRequestParameter("env","dev"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.
+                        path("/currency/**")
+                        .uri("lb://currency-service"))
+                .route(p -> p
+                        .path("/products/**")
+                        .uri("lb:/product-service"))
+                .build();
+    }
+
+}
